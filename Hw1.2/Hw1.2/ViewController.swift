3b8a6c274536b779699f1d57e77a0afbeca6e43d
sfincks <sfincks666@gmail.com>
2024-03-07 09:35:05 -0800

    Hw1.2

---
+++ Hw1.2/Hw1.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hw1.2
+//
+//  Created by sfincks on 07.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
