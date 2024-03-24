27d51079671059173ececa3541c8ff7597f566a5
sfincks <sfincks666@gmail.com>
2024-03-24 03:12:09 -0700

    init

---
+++ swift_2_final/swift_2_final/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift_2_final
+//
+//  Created by Артур on 24.03.2024.
+//  Copyright © 2024 123. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
